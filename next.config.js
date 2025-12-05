95b5f806a14997f401ae93ebf91c53fdda16d751
lonewolf1005 <samarthv080@gmail.com>
2025-12-05 18:19:34 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true, // Required for static export
-  },
   trailingSlash: true,
 };
 
